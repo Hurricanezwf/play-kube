19208b35aaec01c0f3a93f45f8727595000eba5e
zwf <1094646850@qq.com>
2017-02-16 18:18:13 +0800

    添加启动和停止脚本

---
+++ start.sh
@@ -0,0 +1,5 @@
+kubeadm init --use-kubernetes-version v1.5.1
+
+kubectl create -f weave-kube/weave-kube.yaml
+
+echo "\033[32m start ok!\033[0m"
---
+++ stop.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+kubectl delete -f weave-kube/weave-kube.yaml
+kubeadm reset
+
+docker rm $(docker ps -q -a)
+
+echo "\033[32mstop ok!\033[0m"
